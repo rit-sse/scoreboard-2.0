55dec3dbe476db2ef8a32841c53c2ec835e1c29e
Kristen Mills <kristen@kristen-mills.com>
2016-01-16 16:55:15 -0800

    Fixed auth to look for primaryOfficer instead of primary

--- app/js/reducers/auth.js
+++ app/js/reducers/auth.js
@@ -8,7 +8,7 @@ import {
 export default function auth(state = { signedIn: false }, action) {
   switch (action.type) {
   case SIGN_IN_SUCCESS:
-    return { signedIn: true, primary: action.officer.primary };
+    return { signedIn: true, primary: action.officer.primaryOfficer };
   case SIGN_OUT_SUCCESS:
     return { signedIn: false };
   default:
