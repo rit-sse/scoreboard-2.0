23b832659c61ee11629d64b85bc33ace7fb4ac60
Dylan Vuz <vuzdylan@gmail.com>
2016-03-29 09:54:57 -0400

    update gulp

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp';
 import paths from  '../config';
 
-gulp.task('default', ['start'], () => {
+gulp.task('watch', ['build'], () => {
   gulp.watch(paths.source.scripts, ['build:scripts']);
   gulp.watch([paths.source.css], ['build:css']);
   gulp.watch([paths.source.html], ['build:html']);
